61c3d3dfda69570df4f5b611ea37e80fa60e1e59
Anton Rusak <anton.rusak@perfectart.com>
2025-09-10 16:51:09 +0200

    chore: additional logging for contentful SDK

--- src/lib/config/contentfulClient.js
+++ src/lib/config/contentfulClient.js
@@ -4,7 +4,17 @@ if (!process.env.CONTENTFUL_SPACE_ID || !process.env.CONTENTFUL_ACCESS_TOKEN) {
   throw new Error('Contentful credentials are not set')
 }
 
+console.info(
+  'Using Contentful vars',
+  process.env.CONTENTFUL_SPACE_ID?.length,
+  process.env.CONTENTFUL_ACCESS_TOKEN?.length,
+)
+
 export const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  logHandler: (level, data) => {
+    if (level === 'error') console.error('Contentful-SDK', data)
+    else console.log('Contentful-SDK', data)
+  },
 })
